f29663949c513c83704ea46dc53425b367ecade3
Katie Pustolski <pustolskik@gmail.com>
2017-02-16 12:16:35 -0800

    Continued working on tutorial . Code is janky, but it works!

--- Assets/Scripts/TutorialScripts/TutorialBase.cs
+++ Assets/Scripts/TutorialScripts/TutorialBase.cs
@@ -2,34 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialBase : MonoBehaviour {
 
 	public Canvas tutorialCanvas;
 	public Text instructionsTxt;
-	// array of UI image objects in the tutorial canvas.
-	public Image[] tutImages;
+	public GameObject leftArrow;
+	public GameObject rightArrow;
+	public GameObject centerImg;
+
+	// keeps track of what screen to turn off tutorial canvas.
+	// Because arrays start at 0, make sure you add +1 to the screen you want to stop at so the arrays line up.
+	// Eg. if you want to stop at screen 3, place a 4 into this array.
 	public int[] turnOffTutCanvasAtScreen;
-//	public int turnOffTutScreenAtOne=2;
-//	public int turnOffTutScreenAtTwo=4;
+
 	public Image fillMeter;
+
+	// keeps track of screens
+	private int t;
+
 	// Each screen will have instructions
-	// Icons are optional
 	[System.Serializable]
 	public struct TutorialScreens{
 		public string screenName;
 		public string instructions;
-		public Sprite[] icons;
-
+		public AnimationClip animation;
+		public Sprite sprite;
+		public bool isLeftArrowOn;
+		public bool isRightArrowOn;
+		public bool isTutorialDone;
 	}
 
 	public TutorialScreens[] tutScreenArray;
 	private int currentTutorialScreen=0;
+
 	// Use this for initialization
 	void Start () {
+		// stops the game at the beginning
 		Time.timeScale = 0;
-//		switchOnCanvas (true);
 
+		// call change screen to start
 		ChangeScreen ();
 	}
 	
@@ -38,9 +51,15 @@ public class TutorialBase : MonoBehaviour {
 		// Automatically see tutorial canvas at the beginning
 		turnOnTutScreens (turnOffTutCanvasAtScreen[0]);
 
-		//Debug.Log ("fillMeter.GetComponent<Image> ().fillAmount: "+ fillMeter.GetComponent<Image> ().fillAmount);
-		if (fillMeter.GetComponent<Image> ().fillAmount > .70f && fillMeter.GetComponent<Image> ().fillAmount < .90 && currentTutorialScreen < turnOffTutCanvasAtScreen[1]) {
+		// Put conditions to when you want to start/ stop tutorial canvas here!
+
+		// Turn on the tutorial canvas when the fillmeter is between 60% and 90%. Turn it off at turnOffTutCanvasAtScreen index 1;
+		if (fillMeter.GetComponent<Image> ().fillAmount > .60f && fillMeter.GetComponent<Image> ().fillAmount < .90 && currentTutorialScreen < turnOffTutCanvasAtScreen[1]) {
 			turnOnTutScreens (turnOffTutCanvasAtScreen [1]);
+		}
+			
+		if (fillMeter.GetComponent<Image> ().fillAmount <.50f && currentTutorialScreen >= turnOffTutCanvasAtScreen[1] && currentTutorialScreen < turnOffTutCanvasAtScreen[2]) {
+			turnOnTutScreens (turnOffTutCanvasAtScreen [2]);
 		}
 			
 	}
@@ -62,33 +81,54 @@ public class TutorialBase : MonoBehaviour {
 	private void ChangeScreen(){
 		// make sure you don't get an array index out of range error
 		if(currentTutorialScreen != tutScreenArray.Length){
-			int t = currentTutorialScreen;
-//			Debug.Log ("currentTutorialScreen: "+ currentTutorialScreen);
-//			Debug.Log ("tutScreenArray.Length: "+ tutScreenArray.Length);
-//	//		if (currentTutorialScreen <= tutScreenArray.Length - 1) {
-				instructionsTxt.text = tutScreenArray [t].instructions;
 
+			// keep track of current screen
+			t = currentTutorialScreen;
 
+			instructionsTxt.text = tutScreenArray [t].instructions;
+
+//			if (tutScreenArray[t].animation != null) {
+//				centerImg.GetComponent<Animation> ().clip = tutScreenArray [t].animation;
+//			}else {
+			centerImg.GetComponent<Image>().sprite = tutScreenArray[t].sprite;
+//			}
+
+			if (tutScreenArray [t].isLeftArrowOn) {
+				leftArrow.SetActive (true);
+			} else {
+				leftArrow.SetActive (false);
+			}
+
+			if (tutScreenArray [t].isRightArrowOn) {
+				rightArrow.SetActive (true);
+			} else {
+				rightArrow.SetActive (false);
+			}
+
+			if (tutScreenArray[t].isTutorialDone) {
+				SceneManager.LoadScene("Menu");
+			}
 				// loop through icons and assign them to images on the canvas
 				// as long as both arrays are not null
-				if (tutScreenArray [t].icons.Length != 0) {
-
-					for (int i = 0; i < tutImages.Length; i++) {
-						if (i >= tutScreenArray [t].icons.Length) {
-							break;
-						}
-						tutImages [i].enabled = true;
-						Debug.Log ("i: " + i + " tutImages.Length: " + tutImages.Length);
-						Debug.Log ("i: " + i + " tutScreenArray [t].icons: " + tutScreenArray [t].icons.Length);
-						tutImages [i].sprite = tutScreenArray [t].icons [i];
-					}
-				} else {
-					foreach (Image img in tutImages) {
-						img.enabled = false;
-						Debug.Log ("Img: " + img + " bool: " + img.enabled);
-					}
-				}
+//				if (tutScreenArray [t].icons.Length != 0) {
+//
+//					for (int i = 0; i < tutImages.Length; i++) {
+//						if (i >= tutScreenArray [t].icons.Length) {
+//							break;
+//						}
+//						tutImages [i].enabled = true;
+//						Debug.Log ("i: " + i + " tutImages.Length: " + tutImages.Length);
+//						Debug.Log ("i: " + i + " tutScreenArray [t].icons: " + tutScreenArray [t].icons.Length);
+//						tutImages [i].sprite = tutScreenArray [t].icons [i];
+//					}
+//				} else {
+//					foreach (Image img in tutImages) {
+//						img.enabled = false;
+//						Debug.Log ("Img: " + img + " bool: " + img.enabled);
+//					}
+//				}
 		}
+			
 
 	}
 	
@@ -111,7 +151,6 @@ public class TutorialBase : MonoBehaviour {
 		// switch the tutorial canvas off
 		if (currentTutorialScreen >= turnOffTutScreenNum && tutorialCanvas.enabled==true) {
 			switchOnCanvas (false);
-
 		}
 		else {
 			if (DetectTouch ()) {
